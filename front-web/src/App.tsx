41c4eed757b28ac470d2bf1cf4ead90210a1cf83
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-01-07 20:46:13 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
